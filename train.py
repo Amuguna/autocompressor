9f2aa5042da5d38ca9a095f89ba0c2dbe62105bc
Amuguna <junjang99@naver.com>
2026-01-05 00:27:58 -0500

    summary length

--- train.py
+++ train.py
@@ -39,12 +39,17 @@ def main():
     # We now keep distinct sets of args, for a cleaner separation of concerns.
 
     parser = HfArgumentParser((ModelArguments, DataTrainingArguments, TrainingArguments))
+    remaining_args = []
     if len(sys.argv) == 2 and sys.argv[1].endswith(".json"):
         # If we pass only one argument to the script and it's the path to a json file,
         # let's parse it to get our arguments.
         model_args, data_args, training_args = parser.parse_json_file(json_file=os.path.abspath(sys.argv[1]))
     else:
-        model_args, data_args, training_args = parser.parse_args_into_dataclasses()
+        model_args, data_args, training_args, remaining_args = parser.parse_args_into_dataclasses(
+            return_remaining_strings=True
+        )
+        if remaining_args:
+            logger.warning(f"Ignoring unused CLI arguments: {remaining_args}")
 
     # Setup logging
     logging.basicConfig(
